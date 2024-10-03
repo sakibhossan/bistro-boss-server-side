25d8aa2ae80a261b4ddacd3174d93fe34f7a4049
sakib213 <sakibhss438@gmail.com>
2024-10-04 00:07:18 +0600

    browse collection add

--- index.js
+++ index.js
@@ -24,12 +24,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const menucollection = client.db("resturentDb").collection("menu");
+    const reviewscollection = client.db("resturentDb").collection("reviews");
+    app.get('/menu',async(req,res)=>{
+      const result = await menucollection.find().toArray();
+      res.send(result);
+    })
+    app.get('/reviews',async(req,res)=>{
+      const result = await reviewscollection.find().toArray();
+      res.send(result);
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
