5d7092157edd0c351f5276cc24ee6a49c3117eeb
sakib213 <sakibhss438@gmail.com>
2024-11-01 23:37:07 +0600

    dashoboard admin role and create new user and set up exixting  manualy api server in mongodb

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w8b4lyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,8 +25,27 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const menucollection = client.db("resturentDb").collection("menu");
+    const usercollection = client.db("resturentDb").collection("users");
     const reviewscollection = client.db("resturentDb").collection("reviews");
     const cardscollection = client.db("resturentDb").collection("cards");
+    // uesr related api
+    app.get('/users',async(req,res)=>{
+       const result = await usercollection.find().toArray();
+       res.send(result);
+    })
+    
+    app.post('/users',async(req,res)=>{
+      const user = req.body;
+      // inser email if user doesn't exixts
+      // you can dot this many ways (1.email unique,2.upsert,3.Simple Checking)
+      const query = {email: user.email}
+      const exisytingUser = await usercollection.findOne(query);
+      if(exisytingUser){
+        return res.send({message:'user already exists',insertedId:null})
+      }
+      const result = await usercollection.insertOne(user);
+      res.send(result);
+    })
     app.get('/menu',async(req,res)=>{
       const result = await menucollection.find().toArray();
       res.send(result);
@@ -48,6 +67,12 @@ async function run() {
       const result = await cardscollection.insertOne(cardsItem);
       res.send(result)
 
+    });
+    app.delete('/carts/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cardscollection.deleteOne(query);
+      res.send(result);
     })
 
     
